c4eb3bb077163559ddf2d5a58430e25bac25973d
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-17 13:36:39 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,10 +12,16 @@ class Cache:
     class cache
     """
     def __init__(self):
+        """
+        initializes a cache obj
+        """
         self._redis = redis.Redis()
-        self._redis.flushdb
+        self._redis.flushdb()
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        stored data with a generated key
+        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
