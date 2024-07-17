6103789970e482a7409030b7e75b0dda6745b13c
Etsybt <nouhailahmidouch2@gmail.com>
2024-07-17 14:10:06 +0000

    update

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -10,44 +10,32 @@ from functools import wraps
 
 def count_calls(method: Callable) -> Callable:
     """
-    takes a single method Callable
-    argument and returns a Callable
+    Decorator to count calls to a method.
     """
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        """
-        called method
-        """
         key = method.__qualname__
         self._redis.incr(key)
         return method(self, *args, **kwargs)
-    return wrapperself._redis.incr(key)
+    return wrapper
 
 
 class Cache:
     """
-    class cache
+    Cache class using Redis.
     """
+
     def __init__(self):
-        """
-        initializes a cache obj
-        """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        stored data with a generated key
-        """
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
     def get(self, key: str, fn: Optional[Callable] = None) -> any:
-        """
-        takes a key string argument and
-        an optional Callable argument named fn
-        """
         data = self._redis.get(key)
         if data is None:
             return None
@@ -56,13 +44,13 @@ class Cache:
         return data
 
     def get_str(self, key: str) -> Optional[str]:
-        """
-        converts to a string
-        """
-        return data.decode('utf-8')
+        data = self.get(key)
+        if data is not None:
+            return data.decode('utf-8')
+        return None
 
     def get_int(self, key: str) -> Optional[int]:
-        """
-        converts to an int
-        """
-        return int(data)
+        data = self.get(key)
+        if data is not None:
+            return int(data)
+        return None
